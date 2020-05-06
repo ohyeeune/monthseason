e8e548b5eba144f6c0a79e6d1d1533a3f78c4c1c
ohyeeune <s2018s33@e-mirim.hs.kr>
2020-05-06 15:42:19 +0900

    200506 실습보고서 문제 6 사용자에게 월 입력 계절 출력 - Contains() 사용

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,6 +10,28 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            String month;
+            Console.Write("몇 월? : ");
+            month = Console.ReadLine();
+
+            if(month.Contains("12") || month.Contains("1") && month.Contains("2"))
+            {
+                Console.WriteLine("겨울입니다~");
+            } else if (month.Contains("3") || month.Contains("4") || month.Contains("5"))
+            {
+                Console.WriteLine("봄입니다~");
+            }
+            else if (month.Contains("6") || month.Contains("7") || month.Contains("8"))
+            {
+                Console.WriteLine("여름입니다~");
+            }
+            else if (month.Contains("9") || month.Contains("10") || month.Contains("11"))
+            {
+                Console.WriteLine("가을입니다~");
+            } else
+            {
+                Console.WriteLine("잘못 입력하셨어요!");
+            }
         }
     }
 }
